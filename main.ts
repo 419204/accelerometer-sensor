110038a138b31d55ae46460be1b033ae11c4966c
419204 <115142418+419204@users.noreply.github.com>
2023-01-05 16:46:08 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,4 @@
-input.onButtonPressed(Button.A, function () {
-	
-})
-input.onButtonPressed(Button.B, function () {
-	
-})
+let 目標物 = 0
 let accy = 0
 let accx = 0
 let x = 2
@@ -12,4 +7,26 @@ led.plot(x, y)
 basic.forever(function () {
     accx = input.acceleration(Dimension.X)
     accy = input.acceleration(Dimension.Y)
+    if (accx > 200 && x > 0) {
+        x = x - 1
+    }
+    if (accx < -200 && x < 4) {
+        x = x + 1
+    }
+    if (accy > 200 && y > 0) {
+        y = y - 1
+    }
+    if (accy < -200 && y < 4) {
+        y = y + 1
+    }
+    basic.clearScreen()
+    led.plot(x, y)
+    basic.pause(500)
+})
+basic.forever(function () {
+    目標物 = 0
+    led.plot(0, 0)
+    if (true) {
+    	
+    }
 })
